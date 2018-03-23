360338939993e046b94169f5be3618aa570e9423
Annie Mathis <t.anniemathis@gmail.com>
2018-03-22 20:28:08 -0500

    added page templates

--- main.lua
+++ main.lua
@@ -4,11 +4,13 @@
 widget = require 'widget'
 local composer = require('composer') -- needed for scenes
 
+--[[
 -- init game; opens with fade
 local opt = {
   effect = "fade",
   time = 1500
 }
+]]--
 
 -- this is how we'll initially open the login screen
--- composer.gotoScene("login_screen", opt)
+composer.gotoScene("login", opt)
