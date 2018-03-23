98d35f779f5a2a9806b01003a27ede5c7c4f436e
Annie Mathis <t.anniemathis@gmail.com>
2018-03-22 19:30:10 -0500

    added scene template and put some stuff in main

--- main.lua
+++ main.lua
@@ -1,1 +1,14 @@
-print('boop')
+-- this is how you connect the current file to another file
+-- require 'new_game'
+
+widget = require 'widget'
+local composer = require('composer') -- needed for scenes
+
+-- init game; opens with fade
+local opt = {
+  effect = "fade",
+  time = 1500
+}
+
+-- this is how we'll initially open the login screen
+-- composer.gotoScene("login_screen", opt)
